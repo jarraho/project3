c1f3568ef4f86f80573cd16af716c81f867e1d7c
jarraho <aljarraho18@gmail.com>
2021-08-18 16:11:48 +0200

    commit16

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchMovies from "./searchMovies";
+import App from "./App"
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1 className="title">React Movie Search</h1>
-        <SearchMovies/>
-      </div>
-    );
-  }
-}
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+
+ReactDOM.render(<App />,
+                              
+                          document.getElementById('root'));
+
